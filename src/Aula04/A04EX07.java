e4a9ddbcd54c2deda40766e7bd5159f3d368cfdd
tassiosantos <tassiosantos@gmail.com>
2023-07-19 21:50:58 -0300

    Aula 4, ultimo exercício;

---
+++ src/Aula04/A04EX07.java
@@ -0,0 +1,58 @@
+package Aula04;
+
+public class A04EX07 {
+    public static void main(String[] args) {
+        int[][] entradas = {
+                {2, 2, 5},
+                {3, 3, 5},
+                {2, 2, 2},
+                {2, 5, 2},
+                {2, 4, 3},
+                {150, 120, 220},
+                {122, 252, 130},
+                {152, 200, 351},
+                {1232, 2200, 120},
+                {1, 2, 1},
+                {2, 1, 2}
+        };
+
+        String[] saidas = {
+                "LadosInvalidosException",
+                "true",
+                "true",
+                "LadosInvalidosException",
+                "true",
+                "true",
+                "LadosInvalidosException",
+                "true",
+                "LadosInvalidosException",
+                "LadosInvalidosException",
+                "true"
+        };
+
+        for (int i = 0; i < entradas.length; i++) {
+            String resultado = "true";
+
+            try {
+                eUmTriangulo(entradas[i]);
+            } catch (LadosInvalidosException e) {
+                resultado = "LadosInvalidosException";
+            }
+            String esperado = saidas[i];
+
+            System.out.println("Resultado: " + resultado);
+            System.out.println("Esperado: " + esperado);
+            System.out.println(resultado == esperado);
+            System.out.println();
+        }
+    }
+
+    public static void eUmTriangulo(int[] input) throws LadosInvalidosException {
+        int a = input[0];
+        int b = input[1];
+        int c = input[2];
+        if(!((a + b > c)&&(a + c >b)&&(b + c > a))){
+            throw new LadosInvalidosException("Lados não formam triangulo");
+        }
+    }
+}
---
+++ src/Aula04/LadosInvalidosException.java
@@ -0,0 +1,7 @@
+package Aula04;
+
+public class LadosInvalidosException extends Exception {
+    public LadosInvalidosException(String msg){
+        super(msg);
+    }
+}
