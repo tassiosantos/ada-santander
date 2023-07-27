39f0455fcb7e86731b031b5b35a116d30d0a6367
tassiosantos <tassiosantos@gmail.com>
2023-07-26 21:30:52 -0300

    Aula 7, fix:exercicio2 terminado;

--- src/aula07/A07EX02.java
+++ src/aula07/A07EX02.java
@@ -2,10 +2,24 @@ package aula07;
 
 public class A07EX02 {
     public static void main(String[] args) {
-        System.out.println(fatorial(4));
+        int limite = 6;
+        System.out.println(fatorial(limite));
+        System.out.println(fatorialRecursivo(limite));
     }
 
     public static int fatorial(int limite){
+        int fatorial = 1;
+        for(int i = limite; i >= 0; i--){
+            if(i == 0)
+                return fatorial;
+
+            fatorial = fatorial*i;
+        }
+        return fatorial;
+    }
+
+
+    public static int fatorialRecursivo(int limite){
         if(limite == 0)
             return 1;
 
